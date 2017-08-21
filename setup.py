f324ea33d06ea103e02083707057144ecd06e1e4
Phil Ewels <phil.ewels@scilifelab.se>
2017-08-21 17:13:51 +0200

    Attribution

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ MegaQC is a web application that collects results from multiple runs of MultiQC 
 
 See the MultiQC website for installation instructions and documentation: http://multiqc.info/db/
 
-MegaQC was written by Phil Ewels (http://phil.ewels.co.uk) at SciLifeLab Sweden (http://www.scilifelab.se)
+MegaQC was written by Phil Ewels (http://phil.ewels.co.uk) and Denis Moreno at SciLifeLab Sweden (http://www.scilifelab.se)
 """
 
 from setuptools import setup
