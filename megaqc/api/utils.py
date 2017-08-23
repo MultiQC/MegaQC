b78c46da751edea966f0fdc55e192f72882b0e11
Denis Moreno <denis.moreno@scilifelab.se>
2017-08-23 15:33:28 +0200

    small fix for plot category data

--- megaqc/api/utils.py
+++ megaqc/api/utils.py
@@ -124,9 +124,9 @@ def handle_report_data(user, report_data):
                         data_key = report_data['report_plot_data'][plot]['config']['ylab']
 
                     existing_category = db.session.query(PlotCategory).filter(PlotCategory.category_name==data_key).first()
+                    data=json.dumps({x:y for x,y in sub_dict.items() if x != 'data'})
                     if not existing_category:
                         category_id = PlotCategory.get_next_id()
-                        data=json.dumps({x:y for x,y in sub_dict.items() if x != 'data'})
                         existing_category = PlotCategory(plot_category_id=PlotCategory.get_next_id(),
                                                     report_id=report_id,
                                                     config_id=config_id,
