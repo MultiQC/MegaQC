9b7c233a1c8865a5be6e97f12cab5039c1c31638
Phil Ewels <phil.ewels@scilifelab.se>
2017-10-31 15:22:42 +0100

    Need an app variable for gunicorn

--- megaqc/wsgi.py
+++ megaqc/wsgi.py
@@ -1,9 +1,11 @@
 import os
 from megaqc.app import create_app
 from megaqc.settings import TestConfig, DevConfig, ProdConfig
+
 CONFIG = TestConfig()
 if os.environ.get('MEGAQC_DEBUG', False):
     CONFIG = DevConfig()
 elif os.environ.get('MEGAQC_PRODUCTION', False):
     CONFIG = ProdConfig()
-create_app(CONFIG)
+
+app = create_app(CONFIG)
