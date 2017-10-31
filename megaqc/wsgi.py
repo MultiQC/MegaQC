0b58b063b78fd3956532a7a3f788eaa3c7d32ff8
Phil Ewels <phil.ewels@scilifelab.se>
2017-10-31 15:13:52 +0100

    Update wsgi script to check what type of config to use

--- megaqc/wsgi.py
+++ megaqc/wsgi.py
@@ -1,3 +1,9 @@
+import os
 from megaqc.app import create_app
-
-app = create_app()
+from megaqc.settings import TestConfig, DevConfig, ProdConfig
+CONFIG = TestConfig()
+if os.environ.get('MEGAQC_DEBUG', False):
+    CONFIG = DevConfig()
+elif os.environ.get('MEGAQC_PRODUCTION', False):
+    CONFIG = ProdConfig()
+create_app(CONFIG)
