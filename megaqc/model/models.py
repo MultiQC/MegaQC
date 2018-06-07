299552bdc8ed2cc093e655781aad4324c0590e32
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2018-06-07 12:49:02 +0200

    remove old get_next_id methods

--- megaqc/model/models.py
+++ megaqc/model/models.py
@@ -29,10 +29,6 @@ class Report(db.Model, CRUDMixin):
     created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
     uploaded_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(Report.report_id)).first()[0] or 0) + 1
-
     def __init__(self, **kwargs):
         """Create instance."""
         db.Model.__init__(self, **kwargs)
@@ -42,21 +38,16 @@ class Report(db.Model, CRUDMixin):
         return '<Report({rid!r})>'.format(rid=self.report_id)
 
 
-
-
 class ReportMeta(db.Model, CRUDMixin):
-    __tablename__ =  'report_meta'
+    __tablename__ = 'report_meta'
     report_meta_id = Column(Integer, primary_key=True)
     report_meta_key = Column(String(80), nullable=False)
     report_meta_value = Column(String(1024), nullable=False)
     report_id = Column(Integer, ForeignKey('report.report_id'), index=True)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(ReportMeta.report_meta_id)).first()[0] or 0) + 1
 
 class PlotConfig(db.Model, CRUDMixin):
-    __tablename__ =  'plot_config'
+    __tablename__ = 'plot_config'
     config_id = Column(Integer, primary_key=True)
     config_type = Column(String(80),  nullable=False)
     config_name = Column(String(80), nullable=False)
@@ -65,9 +56,6 @@ class PlotConfig(db.Model, CRUDMixin):
 
     fav_users = db.relationship('User', secondary=user_plotconfig_map, backref="favourite_plotconfigs")
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(PlotConfig.config_id)).first()[0] or 0) + 1
 
 class PlotData(db.Model, CRUDMixin):
     __tablename__ = "plot_data"
@@ -78,9 +66,6 @@ class PlotData(db.Model, CRUDMixin):
     sample_id = Column(Integer, ForeignKey('sample.sample_id'), index=True)
     data = Column(String, nullable=False)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(PlotData.plot_data_id)).first()[0] or 0) + 1
 
 class PlotCategory(db.Model, CRUDMixin):
     __tablename__ = "plot_category"
@@ -90,9 +75,6 @@ class PlotCategory(db.Model, CRUDMixin):
     category_name = Column(String(128), nullable=True)
     data = Column(String(2048), nullable=False)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(PlotCategory.plot_category_id)).first()[0] or 0) + 1
 
 class PlotFavourite(db.Model, CRUDMixin):
     __tablename__ = "plot_favourite"
@@ -104,9 +86,6 @@ class PlotFavourite(db.Model, CRUDMixin):
     data = Column(String(2048), nullable=False)
     created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(PlotFavourite.plot_favourite_id)).first()[0] or 0) + 1
 
 class Dashboard(db.Model, CRUDMixin):
     __tablename__ = "dashboard"
@@ -118,9 +97,6 @@ class Dashboard(db.Model, CRUDMixin):
     modified_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
     created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(Dashboard.dashboard_id)).first()[0] or 0) + 1
 
 class SampleDataType(db.Model, CRUDMixin):
     __tablename__ = "sample_data_type"
@@ -137,23 +113,16 @@ class SampleData(db.Model, CRUDMixin):
     sample_id = Column(Integer, ForeignKey('sample.sample_id'), index=True)
     value = Column(String(1024))
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(SampleData.sample_data_id)).first()[0] or 0) + 1
-
 
 class Sample(db.Model, CRUDMixin):
-    __tablename__ ="sample"
+    __tablename__ = "sample"
     sample_id = Column(Integer, primary_key=True)
     sample_name = Column(String(80))
     report_id = Column(Integer, ForeignKey('report.report_id'), index=True)
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(Sample.sample_id)).first()[0] or 0) + 1
 
 
 class SampleFilter(db.Model, CRUDMixin):
-    __tablename__="sample_filter"
+    __tablename__ = "sample_filter"
     sample_filter_id = Column(Integer, primary_key=True)
     sample_filter_name = Column(String(80))
     sample_filter_tag = Column(String(80))
@@ -161,12 +130,9 @@ class SampleFilter(db.Model, CRUDMixin):
     sample_filter_data = Column(String(2048), nullable=False)
     user_id = Column(Integer, ForeignKey('users.user_id'), index=True)
 
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(SampleFilter.sample_filter_id)).first()[0] or 0) + 1
 
 class Upload(db.Model, CRUDMixin):
-    __tablename__="uploads"
+    __tablename__ = "uploads"
     upload_id = Column(Integer, primary_key=True)
     status = Column(String(80), index=True)
     path = Column(String(2048))
@@ -174,7 +140,3 @@ class Upload(db.Model, CRUDMixin):
     created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
     modified_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
     user_id = Column(Integer, ForeignKey('users.user_id'))
-
-    @staticmethod
-    def get_next_id():
-        return (db.session.query(func.max(Upload.upload_id)).first()[0] or 0) + 1
