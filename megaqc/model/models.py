83f9d8180c1d4a8f23db2c4fc369f387ce7dc6ec
Michael Milton <ttmigueltt@gmail.com>
2019-06-13 16:00:31 +1000

    Alert model and API

--- megaqc/model/models.py
+++ megaqc/model/models.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 import datetime as dt
 
+from sqlalchemy import event
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, ForeignKey, Column, Boolean, Integer, Float, Unicode, TIMESTAMP, Binary, DateTime, func
+from sqlalchemy import JSON
+import enum
 
 from megaqc.database import CRUDMixin
 from megaqc.extensions import db
 
+import megaqc.api.utils
 
 user_plotconfig_map = db.Table('user_plotconfig_map',
-            db.Column('user_id', Integer, db.ForeignKey('users.user_id')),
-            db.Column('plot_config_id', Integer, db.ForeignKey('plot_config.config_id'))
-            )
+                               db.Column('user_id', Integer, db.ForeignKey('users.user_id')),
+                               db.Column('plot_config_id', Integer, db.ForeignKey('plot_config.config_id'))
+                               )
 
 user_sampletype_map = db.Table('user_sampletype_map',
-            db.Column('user_id', Integer, db.ForeignKey('users.user_id')),
-            db.Column('sample_data_type_id', Integer, db.ForeignKey('sample_data_type.sample_data_type_id'))
-            )
+                               db.Column('user_id', Integer, db.ForeignKey('users.user_id')),
+                               db.Column('sample_data_type_id', Integer,
+                                         db.ForeignKey('sample_data_type.sample_data_type_id'))
+                               )
 
 
 class Report(db.Model, CRUDMixin):
@@ -50,7 +55,7 @@ class ReportMeta(db.Model, CRUDMixin):
 class PlotConfig(db.Model, CRUDMixin):
     __tablename__ = 'plot_config'
     config_id = Column(Integer, primary_key=True)
-    config_type = Column(Unicode,  nullable=False)
+    config_type = Column(Unicode, nullable=False)
     config_name = Column(Unicode, nullable=False)
     config_dataset = Column(Unicode, nullable=True)
     data = Column(Unicode, nullable=False)
@@ -122,6 +127,23 @@ class Sample(db.Model, CRUDMixin):
     sample_name = Column(Unicode)
     report_id = Column(Integer, ForeignKey('report.report_id'), index=True)
 
+    alerts = relationship("Alert", back_populates='sample')
+
+    def generate_alerts(self):
+        """
+        Triggers any alerts that should be created by the creation of this sample
+        """
+
+        # This is awful and the sample filtering should be done in the database
+        sample_alerts = [alert for alert in db.session.query(AlertThreshold).all() if alert.sample_id == self.sample_id]
+        db.session.bulk_save_objects(sample_alerts)
+
+@event.listens_for(Sample, 'alert_insert')
+def new_alert_threshold(mapper, connection, target: Sample):
+    """
+    Whenever a Sample is inserted into the DB, create Alerts for it
+    """
+    target.generate_alerts()
 
 class SampleFilter(db.Model, CRUDMixin):
     __tablename__ = "sample_filter"
@@ -142,3 +164,50 @@ class Upload(db.Model, CRUDMixin):
     created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
     modified_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
     user_id = Column(Integer, ForeignKey('users.user_id'))
+
+
+class AlertThreshold(db.Model, CRUDMixin):
+    __tablename__ = "alert_threshold"
+    alert_threshold_id = Column(Integer, primary_key=True)
+    threshold = Column(JSON)
+    name = Column(Unicode)
+    description = Column(Unicode)
+    created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
+    modified_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
+    importance = Column(Integer)
+
+    alerts = relationship("Alert", back_populates='threshold')
+
+    def calculate_alerts(self):
+        """
+        Triggers any Alerts that should be created by the creation of this alert
+        """
+        samples = megaqc.api.utils.get_samples(self.threshold)
+        alerts = [Alert(sample=sample, threshold=self, message='') for sample in samples]
+        return alerts
+
+    def generate_alerts(self):
+        """
+        Creates any Alerts that should be created by the creation of this alert
+        """
+        db.session.bulk_save_objects(self.calculate_alerts())
+
+
+@event.listens_for(AlertThreshold, 'alert_insert')
+def new_alert_threshold(mapper, connection, target: AlertThreshold):
+    """
+    Whenever an AlertThreshold is inserted into the DB, create Alerts for it
+    """
+    target.generate_alerts()
+
+
+class Alert(db.Model, CRUDMixin):
+    __tablename__ = "alert"
+    alert_id = Column(Integer, primary_key=True)
+    alert_threshold_id = Column(Integer, ForeignKey('alert_threshold.alert_threshold_id'), index=True)
+    sample_id = Column(Integer, ForeignKey('sample.sample_id'), index=True)
+    message = Column(Unicode)
+    created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
+
+    threshold = relationship("AlertThreshold", back_populates='alerts')
+    sample = relationship("Sample", back_populates='alerts')
