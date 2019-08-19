fa278aaf3c15206e2bf1bfca8a5d51e7d05706c2
Michael Milton <ttmigueltt@gmail.com>
2019-08-19 17:13:16 +1000

    Remove the last test that depended on mixer

--- tests/api/test_plot.py
+++ tests/api/test_plot.py
@@ -2,40 +2,23 @@ from flask import url_for
 from plotly.offline import plot
 import json
 
-import megaqc.user.models as user_models
-from megaqc.model import models
+from tests import factories
 
 
-class TestPlotApi:
-    @staticmethod
-    def test_get_trend_series(mix, db, client):
-        # Create 5 reports each with 1 sample. Each has a single field called 'test_field'
-        data_type = mix.blend(models.SampleDataType, data_key='test_field')
-        report = mix.cycle(5).blend(
-            models.Report,
-            created_at=lambda: mix.faker.date_time(),
-            user=mix.blend(user_models.User),
-            samples=lambda: mix.blend(
-                models.Sample,
-                data=lambda: mix.cycle(1).blend(models.SampleData, data_type=data_type,
-                                                value=mix.faker.small_positive_integer)
-            )
+def test_get_trend_series(db, client):
+    # Create 5 reports each with 1 sample. Each has a single field called 'test_field'
+    data_type = factories.SampleDataTypeFactory()
+    report = factories.ReportFactory.create_batch(5, samples__data__data_type=data_type)
+    db.session.add_all(report)
+    db.session.commit()
 
-        )
-        db.session.add_all([
-            data_type,
-            *report
-        ])
+    response = client.get(url_for('rest_api.trend_data', filter=json.dumps([]), fields=json.dumps([data_type.data_key])))
 
-        response = client.get(url_for('rest_api.trend_data', filter=json.dumps([]), fields=json.dumps(['test_field'])))
+    # Check the request was successful
+    assert response.status_code == 200
 
-        # Check the request was successful
-        assert response.status_code == 200
+    # Check that there are 3 series (mean, stdev, raw data)
+    assert len(response.json) == 3
 
-        # Check that there are 3 series (mean, stdev, raw data)
-        assert len(response.json) == 3
-
-        # Test that this is valid plot data
-        plot({'data': response.json}, validate=True, auto_open=False)
-
-
+    # Test that this is valid plot data
+    plot({'data': response.json}, validate=True, auto_open=False)
