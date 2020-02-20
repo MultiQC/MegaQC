570df7f333c4f5b340f1d000475f6a0c0b52730a
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2020-02-20 12:40:02 +0100

    unblack

--- megaqc/database.py
+++ megaqc/database.py
@@ -76,14 +76,17 @@ class Model(CRUDMixin, db.Model):
 class SurrogatePK(object):
     """A mixin that adds a surrogate integer 'primary key' column named ``id`` to any declarative-mapped class."""
 
-    __table_args__ = {"extend_existing": True}
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
 
     @classmethod
     def get_by_id(cls, record_id):
         """Get record by ID."""
-        if any((isinstance(record_id, basestring) and record_id.isdigit(), isinstance(record_id, (int, float))),):
+        if any(
+                (isinstance(record_id, basestring) and record_id.isdigit(),
+                 isinstance(record_id, (int, float))),
+        ):
             return cls.query.get(int(record_id))
         return None
 
@@ -140,4 +143,4 @@ def init_db(url):
     """Initializes the database."""
     db.metadata.bind = engine
     db.metadata.create_all()
-    print("Initialized the database.")
+    print('Initialized the database.')
