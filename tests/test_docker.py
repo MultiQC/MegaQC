06b2307f6396d93d2a2a03cdf46930281f303765
Michael Milton <ttmigueltt@gmail.com>
2020-10-07 16:24:54 +1100

    Add missing file

---
+++ tests/test_docker.py
@@ -0,0 +1,19 @@
+import subprocess
+from pathlib import Path
+
+import requests
+
+
+def test_compose(multiqc_data):
+    # Start the stack
+    deploy = (Path(__file__).parent.parent / "deploy").resolve()
+    subprocess.run(["docker-compose", "up"], cwd=deploy)
+
+    # Create a user
+    requests.post("localhost/users", json={"username": "foo", "email": "foo@bar.com"})
+
+    # Upload the report
+    requests.post(url="localhost/uploads", files={"report": multiqc_data})
+
+    # Request the report back
+    result = requests.get(url="localhost/uploads").json()
