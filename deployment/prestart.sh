4384f1d0665833045a2d12204e99b162dca95555
Michael Milton <ttmigueltt@gmail.com>
2020-10-08 12:09:57 +1100

    Run migrations, ignore errors

--- deployment/prestart.sh
+++ deployment/prestart.sh
@@ -3,9 +3,10 @@
 # Let the DB start
 sleep 10
 
-# Create the DB
-megaqc initdb
+# Create the DB, ignoring errors, such as if the database already exists
+megaqc initdb || true
 
 # Run migrations
 cd megaqc
 export FLASK_APP=wsgi.py
+flask db upgrade
