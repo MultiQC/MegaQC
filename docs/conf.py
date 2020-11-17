42defe5cf7f54cac3d765990bb4d19743a2b211c
Zethson <lukas.heumos@posteo.net>
2020-11-17 16:20:55 +0100

    black
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,20 @@
 # -- Project information -----------------------------------------------------
 
-project = 'MegaQC'
-copyright = '2020, MegaQC Team'
-author = 'MegaQC Team'
+project = "MegaQC"
+copyright = "2020, MegaQC Team"
+author = "MegaQC Team"
 
-release = '0.3.0'
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
 
-extensions = ['recommonmark']
+extensions = ["recommonmark"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
