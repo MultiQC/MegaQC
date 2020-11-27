556323bad16bcf14784050845854d883c0c812a1
Zethson <lukas.heumos@posteo.net>
2020-11-27 16:20:51 +0100

    removed .env reading
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- megaqc/wsgi.py
+++ megaqc/wsgi.py
@@ -6,7 +6,6 @@ from megaqc.app import create_app
 from megaqc.settings import DevConfig, ProdConfig, TestConfig
 
 env = Env()
-env.read_env()
 
 if env.bool("FLASK_DEBUG", False):
     CONFIG = DevConfig()
