8c86219151cf8be9c43fb71044df9d215a92d53c
jethror1 <rainford1995@gmail.com>
2021-12-07 16:54:23 +0000

    add initial megaqc upload script with requirements

---
+++ logger.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+
+FORMATTER = logging.Formatter(
+    "%(asctime)s:%(name)s:%(module)s:%(levelname)s:%(message)s"
+    )
+LOG_FILE = os.environ["MEGAQC_FULL_LOG"]
+
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+
+def get_file_handler():
+    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+
+    logger.propagate = False
+    return logger
---
+++ megaqc_upload.py
@@ -0,0 +1,233 @@
+"""
+Script to download all multiqc_data.jsons from 002 projects in DNAnexus, then
+runs megaqc upload to import into megaqc database
+
+Writes runs downloaded to log file to ensure they aren't downloaded twice
+
+Required env variables:
+- DOWNLOAD_DIR: dir to download jsons to
+- SLACK_TOKEN: slack api token to send notifications
+- AUTH_TOKEN: DNAnexus authorisation token
+- MEGAQC_FULL_LOG: verbose log file to write all running messages to
+- MEGAQC_UPLOAD_LOG: file to write imported runs to, used to record whats uploaded
+"""
+import os
+from pathlib import Path
+import requests
+import subprocess
+import sys
+
+import dxpy as dx
+
+from logger import get_logger
+
+LOG = get_logger("main_log")
+
+
+
+def dx_login():
+
+    """
+    dxpy login user for dxpy function either by .env file or docker env
+
+    Returns: None
+
+    Raises: dx.exceptions.InvalidAuthentication - raised if token is invalid
+    """
+
+    # try to get auth token from env (i.e. run in docker)
+    try:
+        AUTH_TOKEN = os.environ["AUTH_TOKEN"]
+    except Exception as e:
+        LOG.error('No dnanexus auth token detected')
+        slack_notify(
+            "megaqc upload: Error: no dx auth token found", "egg-alerts"
+        )
+        LOG.info('----- Stopping script -----')
+        sys.exit()
+
+    # env variable for dx authentication
+    DX_SECURITY_CONTEXT = {
+        "auth_token_type": "Bearer",
+        "auth_token": AUTH_TOKEN
+    }
+
+    # set token to env
+    LOG.info('dxpy login initiated')
+    dx.set_security_context(DX_SECURITY_CONTEXT)
+
+    try:
+        # test auth token is valid and user authorised
+        dx.api.system_whoami()
+    except dx.exceptions.InvalidAuthentication as e:
+        LOG.error(f"DNAnexus token invalid: {e}")
+        slack_notify(
+            f"megaqc upload: Error DNAnexus token appears to be invalid:\n{e}",
+            "egg-alerts"
+        )
+
+
+def find_002_projects():
+    """
+    Searches for all 002_ DNAnexus projects
+
+    Returns dict of project-id: project-name
+    """
+    projects = dx.search.find_projects(
+        name="002_*", describe=True, name_mode="glob"
+    )
+    projects = dict([
+        (x["describe"]["id"], x["describe"]["name"]) for x in projects
+    ])
+    LOG.info(f'Found {len(projects)} 002 projects')
+
+    return projects
+
+
+def find_jsons():
+    """
+    Searches for all multiqc_data.json filesin any DNAnexus project
+
+    Returns dict of project-id: file-id
+    """
+    jsons = dx.search.find_data_objects(
+        name="multiqc_data.json", describe=True
+    )
+    jsons = dict([
+        (x["describe"]["project"], x["describe"]["id"]) for x in jsons
+    ])
+
+    LOG.info(f"Found {len(jsons)} multiqc_data.json files")
+
+    return jsons
+
+
+def filter_jsons(projects, jsons):
+    """
+    Filter JSONs to only retain those in 002 projects
+
+    Returns list of tuples as (file-id, json-name)
+    """
+    return dict([
+        (jsons[x], projects[x]) for x in jsons if x in projects
+    ])
+
+
+def get_json(file_id, run_name):
+    """
+    Downloads given JSON to specified dir
+
+    Returns: None
+    """
+    dxpy.bindings.dxfile_functions.download_dxfile(
+        file_id, f"{os.environ['DOWNLOAD_DIR']}/{run_name}-multiqc_data.json"
+    )
+
+
+def upload_json(file):
+    """
+    Runs megaqc upload to import json to megaqc database
+
+    Args: file (PosixPath): path to file to upload
+
+    Returns: None
+    """
+    proc = subprocess.run(
+        f"megaqc upload {file}", shell=True, stderr=subprocess.PIPE
+    )
+
+    if proc.returncode != 0:
+        # error running megaqc upload, stop and notify via slack
+        message = (
+            f"Error running megaqc upload.\n Error code: {proc.returncode}"
+            f"\nError: {proc.stderr}"
+        )
+        LOG.error(message)
+        slack_notify(message, 'egg-alerts')
+        sys.exit()
+    else:
+        # successfully uploaded, log and delete
+        LOG.info(f"Successfully uploaded {file} to megaqc database")
+        os.remove(file)
+        LOG.info(f"Deleted file: {file}")
+
+
+def slack_notify(message, channel):
+    """
+    Sends notification to slack, either to egg-alerts or egg-logs.
+    Handles both error in http request and exceptions being raised
+
+    Args:
+        - message (str): message to send to Slack
+        - channel (str): Slack channel to send to (egg-logs / egg-alerts)
+
+    Returns: None
+    """
+    try:
+        response = requests.post('https://slack.com/api/chat.postMessage', {
+            'token': os.environ['SLACK_TOKEN'],
+            'channel': f'#{channel}',
+            'text': message
+        }).json()
+
+        if not response['ok']:
+            LOG.error((
+                f"Error sending POST reuqest to {channel}.\n"
+                f"Original message: {message}"
+            ))
+            sys.exit()
+    except Exception as e:
+        LOG.error(f"Error in POST request to {channel}.\n{e}")
+        sys.exit()
+
+
+def main():
+    os.environ['MEGAQC_UPLOAD_LOG'] = "/home/jethro/Projects/megaQC_uploads/megaqc_uploads.log"
+    os.environ['DOWNLOAD_DIR'] = "/home/jethro/Projects/megaQC_uploads/json_download/"
+
+    if os.path.isfile(os.environ['MEGAQC_UPLOAD_LOG']):
+        with open(os.environ['MEGAQC_UPLOAD_LOG']) as fh:
+            # read in log file, used to check if a run json has been prev. uploaded
+            log_file = fh.read().splitlines()
+    else:
+        # no log file, likely first time running
+        # create new one and set to empty list => download everything
+        os.mknod(os.environ['MEGAQC_UPLOAD_LOG'])
+        log_file = []
+
+    # find multiqc_data.jsons in 002 projects
+    projects = find_002_projects()
+    jsons = find_jsons()
+    filtered_jsons = filter_jsons(projects, jsons)
+
+    LOG.info(f"{len(filtered_jsons)} total JSON files in 002 projects")
+
+    # generate list of downloaded JSONs to add to log file
+    downloaded = []
+
+    for json_file, run in filtered_jsons.items():
+        if run not in log_file:
+            # run name not logged => download
+            get_json(json_file, run)
+            downloaded.append(run)
+            print(f"Downloaded {json_file}")
+        else:
+            print(f"Run {run} already imported, skipping...")
+
+    LOG.info(f"Downloaded {len(downloaded)} JSONs to import")
+
+    # megaqc upload needs to be run from /app/megaqc
+    os.chdir("/app/megaqc/")
+
+    # import each file into database using megaqc upload, then delete files
+    # write each run file imported to log
+    with open(os.environ['MEGAQC_UPLOAD_LOG'], 'a') as fh:
+        for file in Path(os.environ['DOWNLOAD_DIR']).glob('*.json'):
+            upload_json(file)
+            fh.write(f'{file}\n')
+            LOG.info(f"{file} imported to database")
+            os.remove(file)
+
+
+if __name__ == "__main__":
+    main()
