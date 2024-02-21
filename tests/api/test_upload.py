dd105418bb35316bcb6d29350ca7586435db37d8
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2024-02-21 18:49:25 +0100

    Add tests

--- tests/api/test_upload.py
+++ tests/api/test_upload.py
@@ -14,7 +14,21 @@ def upload(session):
     return r
 
 
-def test_post_upload_list(db, client, token):
+def test_get_upload_list(db, client, token):
+    _test_post_upload_list(db, client, token, "multiqc_data.json")
+
+
+def test_get_upload_list_v120(db, client, token):
+    # New MultiQC 1.20 format (Plotly-backed)
+    _test_post_upload_list(db, client, token, "multiqc_data_v120.json")
+
+
+def test_get_upload_list_v120_hc(db, client, token):
+    # New MultiQC 1.20 format (`--template highcharts` legacy HighCharts backend)
+    _test_post_upload_list(db, client, token, "multiqc_data_v120_hc.json")
+
+
+def _test_post_upload_list(db, client, token, fname):
     """
     Test uploading a report.
     """
@@ -22,7 +36,7 @@ def test_post_upload_list(db, client, token):
 
     rv = client.post(
         "/rest_api/v1/uploads",
-        data={"report": resource_stream("tests", "multiqc_data.json")},
+        data={"report": resource_stream("tests", fname)},
         headers={
             "access_token": token,
             "Content-Type": "multipart/form-data",
